9d2e549f7a5c19261b10db8c0427efb77a162b67
villalobos1727 <112015959+villalobos1727@users.noreply.github.com>
2023-01-23 16:49:49 -0300

    Add files via upload

---
+++ Criando-Joias-main/Criando-Joias-main/index.php
@@ -0,0 +1,90 @@
+<?php
+  require_once 'head.php';
+  require_once 'menu.php';
+
+?>  
+     <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <img src="imagens/VJ.png" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container-fluid texto">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h2>Conheça Algumas de Nossas criaçóes</h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid imagens">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="imagens/joias1.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="imagens/joias2.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="imagens/joias3.jpg" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+        <div class="col-md-6 col-sm">
+          <img src="imagens/joias4.jpg" class="img-fluid">
+        </div>
+        <div class="col-md-6 col-sm">
+          <h3>J</h3>
+            <p>.</p>
+            <p>.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+        <div class="col-md-6 col-sm">
+          <h3>H</h3>
+          <p>.</p>
+          <p>. </p>
+        </div>
+        <div class="col-md-6 col-sm">
+          <img src="imagens/joias5.jpg" class="img-fluid">
+          
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+        <div class="col-md-6 col-sm">
+          <img src="imagens/joias7.jpg" class="img-fluid">
+        </div>
+        <div class="col-md-6 col-sm">
+          <h3>S</h3>
+          <p> .</p>
+        </div>
+      </div>
+    </div>
+
+    <?php
+        require_once 'footer.php';
+
+    ?>
+ 
+
+
+
+
+
+   
